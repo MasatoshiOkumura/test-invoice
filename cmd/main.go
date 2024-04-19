8e843d7bc992fa77c9de6da5ab60b5553ec2c7ca
okumura <m.okumura313@gmail.com>
2024-04-19 16:17:30 +0900

    ginの導入

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,23 @@
 package main
 
 import (
+	"test-invoice/handler"
 	"test-invoice/infrastructure"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	infrastructure.Init()
+
+	r := gin.Default()
+
+	invoices := r.Group("api/invoices")
+	{
+		invoiceHandler := handler.InvoiceHandler{}
+		invoices.GET("/", invoiceHandler.List)
+		invoices.POST("/", invoiceHandler.Create)
+	}
+
+	r.Run()
 }
